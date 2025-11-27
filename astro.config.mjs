ebe161d30e7546e9d975816ccda5347b20048b42
mooenz <joss92821@hotmail.com>
2025-11-26 22:51:25 -0500

    Actualizar configuración de Astro y dependencias: se añaden integraciones de sitemap y critters, se ajustan alias y se actualiza package.json y package-lock.json.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import path from 'node:path';
-// import critters from 'astro-critters';
-// import sitemap from '@astrojs/sitemap';
+import critters from 'astro-critters';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://Mooenz.github.io',
-	base: '/portfolio/',
-	// integrations: [critters(), sitemap()],
+	// site: 'https://Mooenz.github.io',
+	// base: '/portfolio/',
+	site: 'https://mooenz.me',
+	base: '/',
+	integrations: [critters(), sitemap()],
 	vite: {
 		plugins: [tailwindcss()],
 		build: {
 			cssCodeSplit: true,
-		},
-		resolve: {
-			alias: {
-				'@': path.resolve('./src'),
-				'@assets': path.resolve('./src/assets'),
-				'@components': path.resolve('./src/components'),
-				'@styles': path.resolve('./src/styles'),
-			},
 		},
 	},
 });
