a32e92e5a9d003935949bc6ce6daf84159d3056b
mooenz <joss92821@hotmail.com>
2025-11-28 12:19:26 -0500

    chore: update dependencies in package.json
    - Added @astrojs/vercel dependency (version ^8.0.4)
    - Moved tailwindcss dependency to a new line for clarity

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,15 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import critters from 'astro-critters';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
 	// site: 'https://Mooenz.github.io',
 	// base: '/portfolio/',
 	site: 'https://mooenz.me',
 	base: '/',
+	output: 'server',
+	adapter: vercel({}),
 	integrations: [critters(), sitemap()],
 	vite: {
 		plugins: [tailwindcss()],
