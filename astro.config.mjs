f290f2dd70852fc3778ec0c2a6480d1f20e1fab4
mooenz <joss92821@hotmail.com>
2025-11-28 12:28:37 -0500

    Actualizar configuración de Astro: cambiar adaptador a vercel estático y modificar salida a estática

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,14 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import critters from 'astro-critters';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
 	// site: 'https://Mooenz.github.io',
 	// base: '/portfolio/',
 	site: 'https://mooenz.me',
 	base: '/',
-	output: 'server',
+	output: 'static',
 	adapter: vercel({}),
 	integrations: [critters(), sitemap()],
 	vite: {
