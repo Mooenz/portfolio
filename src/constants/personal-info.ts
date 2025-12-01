7818abd68a16c89e3def070e4bac3bc98ac1a5ca
mooenz <joss92821@hotmail.com>
2025-12-01 17:00:01 -0500

    Agregar imagen de Open Graph y actualizar metadatos en Layout.astro

--- src/constants/personal-info.ts
+++ src/constants/personal-info.ts
@@ -5,17 +5,15 @@ interface PersonalInfo {
 	readonly expertise: string;
 	readonly github: string;
 	readonly linkedIn: string;
-	readonly image: string;
 }
 
 const personal: PersonalInfo = {
 	name: 'Jose Manuel Montaño Saenz',
-	role: 'Frontend Developer',
+	role: 'Desarrollador frontend',
 	email: 'joss92821@hotmail.com',
 	expertise: '+3 años de experiencia en la industria.  Ingeniero de sistemas Graduado y Especializado en el desarrollo de webs únicas, rápidas, accesibles y modernas.',
 	github: 'https://github.com/Mooenz',
 	linkedIn: 'https://www.linkedin.com/in/mooenz/',
-	image: '',
 };
 
 export default personal;
