a3124cdb0a1fc9b36d44c86c0a5b32e2953190da
Mooenz <joss92821@hotmail.com>
2026-07-03 12:38:17 -0500

    Refactor configuración de Astro y actualizar contenido de SEO; eliminar favicon y ajustar formato de vercel.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
@@ -29,3 +30,36 @@ export default defineConfig({
 		},
 	},
 });
+=======
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+	// site: 'https://Mooenz.github.io',
+	// base: '/portfolio/',
+	site: 'https://www.mooenz.me',
+	base: '/',
+	output: 'static',
+	integrations: [sitemap()],
+	vite: {
+		plugins: [tailwindcss()],
+		build: {
+			cssCodeSplit: true,
+			// Asegura que CSS se cargue antes que JS
+			modulePreload: {
+				polyfill: true,
+			},
+			rollupOptions: {
+				output: {
+					// CSS siempre se carga primero al estar en el head
+					assetFileNames: 'assets/css/[name]-[hash][extname]',
+					chunkFileNames: 'assets/js/[name]-[hash].js',
+					entryFileNames: 'assets/js/[name]-[hash].js',
+				},
+			},
+		},
+	},
+});
+>>>>>>> 676f69f (Refactor configuración de Astro y actualizar contenido de SEO; eliminar favicon y ajustar formato de vercel.json)
--- src/constants/seo.ts
+++ src/constants/seo.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export const SITE_URL = 'https://mooenz.me';
 
 interface seo {
@@ -26,3 +27,30 @@ const seo = {
 };
 
 export default seo;
+=======
+interface seo {
+	readonly title: string;
+	readonly description: string;
+	readonly canonical: string;
+	readonly og: {
+		readonly url: string;
+		readonly locale: string;
+		readonly type: string;
+		readonly image: string;
+	};
+}
+
+const seo = {
+	title: 'Desarrollador Frontend | José Manuel Montaño Saenz',
+	description: 'Portafolio de José Manuel Montaño Saenz, Desarrollador Frontend con más de 3 años de experiencia en la creación de sitios web modernos, rápidos y accesibles.',
+	canonical: 'https://www.mooenz.me/',
+	og: {
+		url: 'https://www.mooenz.me/',
+		type: 'website',
+		locale: 'es_CO',
+		image: 'https://www.mooenz.me/images/og-image.webp',
+	},
+};
+
+export default seo;
+>>>>>>> 676f69f (Refactor configuración de Astro y actualizar contenido de SEO; eliminar favicon y ajustar formato de vercel.json)
